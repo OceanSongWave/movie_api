c8a984f3920ef5399798608977d129b8928da33b
OceanSongWave <65198135+OceanSongWave@users.noreply.github.com>
2020-12-11 19:33:27 -0500

    remove unrelated files

--- circle.js
+++
@@ -1,8 +0,0 @@
-// circle.js
-let PI = 3.14159265359;
-module.exports.area = (radius) => Math.pow(radius, 2) * PI;
-module.exports.circumference = (radius) => 2 * radius * PI;
-
-// const PI = 3.14159265359;
-// export function area(radius) { return radius ** 2 * PI; }
-// export function circumference(radius) { return 2 * radius * PI; }
--- myModule.js
+++
@@ -1,5 +0,0 @@
-console.log('This is a new Module');
-function add(a, b) {
-    return a + b
-}
-console.log(add (2, 3));
