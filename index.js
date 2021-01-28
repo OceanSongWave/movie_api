5c29b5c77750b06877445ad03d061cc78e330f89
OceanSong <65198135+OceanSongWave@users.noreply.github.com>
2021-01-27 19:16:07 -0500

    Added optional connection

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 const { check, validationResult } = require("express-validator");
+
 // local connection
 // mongoose.connect("mongodb://localhost:27017/myFlixDB", {useNewUrlParser: true});
 mongoose.connect(process.env.CONNECTION_URI,
-  // "mongodb+srv://oceansong:rich4Database@starflixdb.ct9jy.mongodb.net/myFlixDB?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
+// mongoose.connect(
+//   "mongodb+srv://oceansong:rich4Database@starflixdb.ct9jy.mongodb.net/myFlixDB?retryWrites=true&w=majority",
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
 
 app.use(morgan("common"));
 app.use(express.static("public"));
